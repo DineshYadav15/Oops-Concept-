d1471d54f3a8674765d85bab48d37a2c1a352fc7
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-09-11 21:41:33 +0530

    add

---
+++ AddTwoNumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class AddTwoNumbers
+{
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s1 = new Scanner(System.in);
+		System.out.println("enter first number : ");
+		int num=s1.nextInt();
+		System.out.println("enter second numbers :");
+		int num1=s1.nextInt();
+         //int a=20;
+		//int b=30;
+		int sum;
+		sum=num+num1;
+		System.out.println("Add two numbers = " +sum);
+		
+	}
+
+}
