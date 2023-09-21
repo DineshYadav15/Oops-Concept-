00e7bc3ca870be7bda14cd537b916d58164b5061
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-09-21 19:05:03 +0530

    n

---
+++ MaxBETWENtwonumbers.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class MaxBETWENtwonumbers
+{
+	public static void main(String args[])
+	{
+		Scanner s1=new Scanner(System.in);
+		System.out.print("Enter the any numbers");
+		int num=s1.nextInt();
+		int num1=s1.nextInt();
+		if(num>num1)
+		{
+			System.out.print("max numbers num");
+		}
+		else
+		{
+			System.out.print("max number num1");
+		}
+	}
+}
