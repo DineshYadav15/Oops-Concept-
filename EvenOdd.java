169ce922c51509abcd60abd4bc7cbe449c9ff1bb
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-09-21 18:44:08 +0530

    even

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class EvenOdd
+{
+	public static void main(String args[])
+	{
+		Scanner s1=new Scanner(System.in);
+		System.out.print("Enter the any numbers");
+		int num=s1.nextInt();
+		if(num%2==0)
+		{
+			System.out.print("it is even number");
+		}
+		else
+		{
+			System.out.print("it is odd numbers");
+		}
+	}
+}
+			
