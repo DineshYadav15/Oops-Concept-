f9f236bf31a4edb2a6b41cb1ea88f4d04aa81e02
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-10-02 16:37:19 +0530

    method overdding example

---
+++ methodovverridingexample.java
@@ -0,0 +1,23 @@
+ class classA
+	{
+		void dis()
+		{
+		System.out.println("i am dinesh yadav");
+		}
+	}
+	class classB  extends classA
+	{
+		void dis()
+		{
+		System.out.println("i am brother of ravi yadav");
+		super.dis();
+		}
+	}
+class methodovverridingexample
+{
+	public static void main(String[] args)
+	{
+	 classB s1=new classB();
+	s1.dis();
+	}
+}
