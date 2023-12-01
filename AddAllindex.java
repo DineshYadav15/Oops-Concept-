43f45213fe5d6f301abed9d349f25f15853af908
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-12-01 07:46:15 +0530

    all index sum in array

---
+++ AddAllindex.java
@@ -0,0 +1,15 @@
+class AddAllindex
+{
+	public static void main(String args[])
+	{
+	int arr[]={1,3,5,7};
+	int sum=0;
+	for(int i=0;i<arr.length;i++)
+	{
+		sum=sum+arr[i];
+	}
+	  System.out.print("all index of sum in array = " + sum);
+	
+	}
+}
+	
