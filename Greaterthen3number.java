04936e5105efd26dbe2895d9c50113c39faf38f7
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-12-27 01:33:27 +0530

    largest three number without array

---
+++ Greaterthen3number.java
@@ -0,0 +1,24 @@
+class Greaterthen3number
+{
+	
+	public static void main(String args[])
+	{
+		int a=30;
+		int b=87;
+		int c=98;
+		if(a>b && a>c)
+		{
+			System.out.println("largest numbers of is = " + a);
+		}
+		else if(b>a && b>c)
+		{
+			System.out.println("largest of three numbers is = " + b);
+		}
+		else
+		{
+			System.out.println("largest of three numbers is = " + c);
+		}
+	}
+}
+			
+			
