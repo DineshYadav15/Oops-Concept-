fe27cebea36071a61139b10208185f77207fff83
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-12-29 01:17:29 +0530

    prictice for armstrong number

--- ArmstrongNo.java
+++ ArmstrongNo.java
@@ -1,28 +1,28 @@
 import java.util.*;
-class ArmstrongN0
+class ArmstrongNo
 {
 	public static void main(String args[])
 	{
 		Scanner s1=new Scanner(System.in);
 		System.out.println("Enter the any numbers");
-		int num =s1.nextInt();
+		int num=s1.nextInt();
 		int r;
 		int sum=0;
-		int s=num;
-		while(num>0);
+	    int h=num;
+		while(num>0)
 		{
 			r=num%10;
 			sum=sum+r*r*r;
 			num=num/10;
 		}
-		if(s==r)
+		if(h==sum)
 		{
-			System.out.println("true");
+			System.out.println("it is Armstrong number");
 			
 		}
 		else
 		{
-			System.out.println("true");
+			System.out.println("it is not Armstrong number");
 		}
 	}
 }
--- armstrongnumber.java
+++ armstrongnumber.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class ArmstrongN0
+class Armstrongnumber
 {
 	public static void main(String args[])
 	{
