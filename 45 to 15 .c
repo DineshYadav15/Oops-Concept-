3f2f3fe8060c167e66134388619d13066c87cbd6
Dinesh Yadav <dineshyadav1518@gmail.com>
2024-01-02 23:20:37 +0530

    practice

---
+++ 45 to 15 .c
@@ -0,0 +1,31 @@
+//program to print from 45 to 15 using do while loop, while loop, for loop;
+/*#include<stdio.h>
+int main()
+{
+	int a=45;
+	do{
+		printf("\n %d",a);
+		a--;
+	}while(a>=15);
+    
+}*/
+/*
+#include<stdio.h>
+int main()
+{
+	int a=46;
+	while(a>=15)
+	{
+		printf("%d",a);
+		a--;
+	}
+}*/
+#include<stdio.h>
+int main()
+{
+	int a;
+	for(a=47;a>=15;a--)
+	{
+		printf("\n %d",a);
+	}
+}
